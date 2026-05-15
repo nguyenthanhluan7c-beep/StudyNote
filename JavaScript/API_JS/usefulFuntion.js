acdb7b80e0e1d774909703cc9700989934df6414
unknown <nguyenthanhluan7c@gmail.com>
2026-05-15 08:13:53 +0700

    xong login(cho cả hai) + register(cho user thôi), đã liên kết các trang với nhau

---
+++ JavaScript/API_JS/usefulFuntion.js
@@ -0,0 +1,11 @@
+//API chứa những hàm sử dụng nhiều 
+//========================================================
+//  kho chứa biến 
+const default_img = [
+    ""
+]
+//========================================================
+//hàm dùng để load hình ảnh + thông tin người dùng từ data
+function loadInfomatin(userData, loadingLocation) {
+
+}
